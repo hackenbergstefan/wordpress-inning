7f4fc57f7220b49f6fcc66f45ab59108adf7bedc
maximilianhuber <mail@maximilian-huber.de>
2015-05-13 19:30:39 +0200

    suppress some output

--- pullAll.sh
+++ pullAll.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-pushd "`dirname "$0"`"
+pushd "`dirname "$0"`" >/dev/null
 git pull origin master
 git submodule foreach git pull origin master
 for SUBM in ./wordpress/wp-content/themes/wordpress-inning-theme; do
-  pushd $SUBM
+  pushd $SUBM >/dev/null
   git submodule update --remote --merge
   popd
   git add $SUBM
--- pushAll.sh
+++ pushAll.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pushd "`dirname "$0"`"
+pushd "`dirname "$0"`" >/dev/null
 for SUBM in ./wordpress/wp-content/themes/wordpress-inning-theme; do
   git add $SUBM
   git commit -m "update submodule $(basename $SUBM)" $SUBM
