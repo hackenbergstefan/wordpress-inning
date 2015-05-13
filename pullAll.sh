cf43628bf940743879415404fdeeae14999d66d4
maximilianhuber <mail@maximilian-huber.de>
2015-05-13 17:15:52 +0200

    update pullAll.sh script

--- pullAll.sh
+++ pullAll.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 pushd "`dirname "$0"`"
-git pull
-git submodule foreach git pull
+git pull origin master
+git submodule foreach git pull origin master
+for SUBM in ./wordpress/wp-content/themes/wordpress-inning-theme; do
+  pushd $SUBM
+  git submodule update --remote --merge
+  popd
+  git add $SUBM
+  git commit -m "update submodule $(basename $SUBM)" $SUBM
+done
 popd
